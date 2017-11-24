a66e90da419ce947e51fe9e80290353c67670a21
FireInDark <993378951@qq.com>
2017-11-24 11:24:21 +0800

    将ID引用，设置为外键引用

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
+from DjangoUeditor.models import UEditorField
 # Create your models here.
 
 User = get_user_model()
@@ -20,7 +21,7 @@ class UserProfile(AbstractUser):
     avatar = models.ImageField(verbose_name=u'用户头像', upload_to='user/', default='default.jpg', help_text=u"用户头像")
     mobile = models.CharField(verbose_name=u"手机号",unique=True, max_length=11, help_text=u"手机号")
     part = models.IntegerField(verbose_name=u"所属部门", choices=DEPARTMENTS, help_text=u"所属部门")
-    fav_article = models.IntegerField(verbose_name=u"用户收藏文章ID", null=True, blank=True)
+    fav_article = models.ForeignKey('Article',verbose_name=u"用户收藏文章", null=True, blank=True)
 
     def __str__(self):
         return self.username
@@ -40,13 +41,13 @@ class Announcement(models.Model):
     )
     title = models.CharField(verbose_name=u'公告标题', max_length=150, help_text=u"公告标题")
     # 使用富文本改写
-    content = models.TextField(verbose_name=u"公告内容", help_text=u"公告内容")
+    content = UEditorField(verbose_name=u"公告内容",imagePath='announ/images/', filePath="announ/files/",help_text=u"公告内容")
     announ_type = models.IntegerField(verbose_name=u"公告类型", choices=ANNOUN_TYPE, help_text=u"公告类型")
     create_time = models.DateTimeField(verbose_name=u'发布时间', auto_now_add=True, help_text=u"公告发布时间")
     is_published = models.BooleanField(verbose_name=u"是否发布", default=True, help_text=u"是否发布公告")
     update_time = models.DateTimeField(verbose_name=u"修改时间", auto_now=True, help_text=u"修改时间")
     # 发布人
-    publisher = models.IntegerField(verbose_name=u"发布人ID", help_text=u"发布人ID")
+    publisher = models.ForeignKey(User, related_name="announ",verbose_name=u"发布人ID", help_text=u"发布人ID")
 
     def __str__(self):
         return self.title
@@ -70,7 +71,7 @@ class Comments(models.Model):
     parent_id = models.IntegerField(verbose_name=u"所属类型ID", help_text=u"所属类型ID")
     content = models.TextField(verbose_name=u"留言内容", help_text=u"留言内容")
     create_time = models.DateTimeField(verbose_name=u"创建时间", auto_now_add=True)
-    commenter = models.IntegerField(verbose_name=u"留言者ID", help_text=u"留言者ID")
+    commenter = models.ForeignKey(User,related_name='comment',verbose_name=u"留言者", help_text=u"留言者")
     is_published = models.BooleanField(verbose_name=u"是否公开", default=True, help_text=u"是否公开")
     is_readed = models.BooleanField(verbose_name=u"是否被文章发布者阅读", default=False, help_text=u"是否被文章发布者阅读")
 
@@ -81,7 +82,7 @@ class Comments(models.Model):
         return self.comment_type
 
 
-class Article(models.Model):
+class Articles(models.Model):
     """
     博客文章
     """
@@ -94,7 +95,7 @@ class Article(models.Model):
     title = models.CharField(verbose_name="博客标题", max_length=150, help_text=u"博客标题")
     tagname = models.CharField(verbose_name="文章标签", max_length=150, help_text=u"文章标签")
     content = models.TextField(verbose_name="文章正文", help_text=u"文章正文")
-    author = models.IntegerField(verbose_name=u"作者ID", help_text=u"作者ID")
+    author = models.ForeignKey(User, related_name='article', verbose_name=u"作者", help_text=u"作者")
     is_published = models.BooleanField(verbose_name=u"是否公开", default=True, help_text=u"是否公开")
     create_time = models.DateTimeField(u"创建时间", auto_now_add=True)
     update_time = models.DateTimeField(u"修改时间", auto_now=True)
